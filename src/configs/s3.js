acac372a9abde4513372936b6350e6b7393728d7
batdimoiprint <johnkennyreyes21@gmail.com>
2026-05-01 20:08:58 +0800

    fix: s3 config again

--- src/configs/s3.js
+++ src/configs/s3.js
@@ -5,7 +5,9 @@ const s3Config = {
   region: env.AWS_REGION || 'ap-southeast-1',
 };
 
-if (env.AWS_ACCESS_KEY_ID && env.AWS_SECRET_ACCESS_KEY) {
+// In production, we rely on the Lambda IAM Role (don't provide credentials)
+// In development, we use the local .env credentials
+if (env.NODE_ENV !== 'production') {
   s3Config.credentials = {
     accessKeyId: env.AWS_ACCESS_KEY_ID,
     secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
