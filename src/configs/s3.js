2dbb86c0c16805fda544bf5e8d804f24125993fb
batdimoiprint <johnkennyreyes21@gmail.com>
2026-05-01 19:57:05 +0800

    fix: s3 config

--- src/configs/s3.js
+++ src/configs/s3.js
@@ -1,12 +1,17 @@
 import { S3Client } from '@aws-sdk/client-s3';
 import env from './env.js';
 
-const s3Client = new S3Client({
+const s3Config = {
   region: env.AWS_REGION || 'ap-southeast-1',
-  credentials: {
+};
+
+if (env.AWS_ACCESS_KEY_ID && env.AWS_SECRET_ACCESS_KEY) {
+  s3Config.credentials = {
     accessKeyId: env.AWS_ACCESS_KEY_ID,
     secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
-  },
-});
+  };
+}
+
+const s3Client = new S3Client(s3Config);
 
 export default s3Client;
